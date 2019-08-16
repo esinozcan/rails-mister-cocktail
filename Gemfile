04b094830aa90b88f3f4daf3527d46cdf6cfd57a
Esin Ozcan <esinozcan@sabanciuniv.edu>
2019-08-16 23:18:38 +0200

    Add Article scaffold

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ ruby '2.6.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
-gem 'bootstrap-sass'
+
 gem 'simple_form'
 
 # Use postgresql as the database for Active Record
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,29 +1,29 @@
 class DosesController < ApplicationController
-
- def new
-    @dose = Dose.new
+  def new
     @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
   end
 
   def create
-    @dose = Dose.new(dose_params)
     @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      render "cocktails/show"
+      render :new
     end
   end
-end
 
-def destroy
+  def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
     redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
-  def dose_params
-    params.require(:dose).permit(:description)
+
+   def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
+end
